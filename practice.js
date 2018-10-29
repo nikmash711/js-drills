36d9b90701dd116e3157e23bc056c6f88967aa70
Nikkie Mashian <nikmash@ucla.edu>
2018-10-29 11:48:21 -0700

    anotha

--- practice.js
+++ practice.js
@@ -1,5 +1,9 @@
+function getYearOfBirth(age){
+    return 2018-age;
+}
+
 function createGreeting(name, age){
-    const yearOfBirth = 2018-age;
+    const yearOfBirth = getYearOfBirth(age);
     return `Hi my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}.`;
 }
 
