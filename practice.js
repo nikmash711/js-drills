240a4974225a43952633d4492155c3a07bd96313
Nikkie Mashian <nikmash@ucla.edu>
2018-10-29 11:59:56 -0700

    peace

--- practice.js
+++ practice.js
@@ -1,10 +1,25 @@
 function getYearOfBirth(age){
-    return 2018-age;
+    if (age<=0){
+        throw new Error("Age can't be negative!");
+    }
+    else{
+        return 2018-age;
+    }
 }
 
 function createGreeting(name, age){
-    const yearOfBirth = getYearOfBirth(age);
-    return `Hi my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}.`;
+    if (name===undefined || age ===undefined){
+        throw new Error("Arguments not valid!");
+    }
+    else{
+        const yearOfBirth = getYearOfBirth(age);
+        return `Hi my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}.`;
+    }
 }
 
-console.log(createGreeting("Nikkie",23));
+try{
+    console.log(createGreeting("Nikkie"));
+}
+catch(e){
+    console.log(e);
+}
